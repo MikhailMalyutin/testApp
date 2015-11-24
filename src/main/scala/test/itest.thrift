68c525459bc0f3dda9a2320cbcb2e21596b68fca
msm <mmalutin@flexis.ru>
2015-11-24 22:30:14 +0300

    Initial interface description

--- src/main/scala/test/itest.thrift
+++ src/main/scala/test/itest.thrift
@@ -7,3 +7,10 @@ struct Tag {
  1: i32 id,
  2: string name
 }
+
+service TestService {
+   i32 addTag(Tag tag),
+   void removeTag(i32 tagId),
+   list<Record> getRecords(list<i32> tags),
+   list<Tag> getTags(i32 recordId)
+}
