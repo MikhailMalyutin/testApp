13da08bb700e7d4a45d2376884678b3cf81994aa
msm <mmalutin@flexis.ru>
2015-11-25 15:05:36 +0300

    Первая моковая реализация

--- src/main/scala/test/server/impl/MainLogic.scala
+++ src/main/scala/test/server/impl/MainLogic.scala
@@ -3,7 +3,7 @@ package test.server.impl
 import com.twitter.util.{Await, Future}
 import test.{Record, Tag, TestService}
 
-object MainLogic extends TestService {
+object MainLogic extends TestService[Future] {
   override def addTag(tag: Tag): Future[Int] = Future.value(0)
 
   override def getRecords(tags: Seq[Int]): Future[Seq[Record]] = Future{Seq.empty}
