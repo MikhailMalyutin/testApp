f5c2676107a5c1ae10c2e131142394ba8ef70fc5
msm <mmalutin@flexis.ru>
2015-11-25 15:22:25 +0300

    Первая моковая реализация

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,13 +2,19 @@ package com.twitter.finagle.example.thrift
 
 import com.twitter.finagle.Thrift
 import com.twitter.util.{Await, Future}
+import test.TestService
 import test.server.impl.MainLogic
 
 object ThriftServer {
   def main(args: Array[String]) {
     //#thriftserverapi
     val server = Thrift.serveIface("localhost:8080", MainLogic)
-    Await.ready(server)
+    val client = Thrift.newIface[TestService.FutureIface]("localhost:8080")
+    val r = client.getRecords()
+    val result = Await.result(r)
+    println(result.toString)
+    print("test ok")
+  //  Await.ready(server)
     print("runned")
     //val client = Thrift.client.
     //#thriftserverapi
