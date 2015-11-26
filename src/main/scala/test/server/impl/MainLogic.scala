52d40422509dc18bbb716b941b411b368abac4db
msm <mmalutin@flexis.ru>
2015-11-26 11:02:12 +0300

    Реализация getRecords похожая на правду и компилируемая

--- src/main/scala/test/server/impl/MainLogic.scala
+++ src/main/scala/test/server/impl/MainLogic.scala
@@ -30,7 +30,7 @@ object MainLogic extends TestService[Future] {
 
   override def getRecords(tags: Seq[Int]): Future[Seq[Record]] = Future{
       val some = tags.flatMap(TagIdRecordsIds.get).flatten
-      some.map{recId => IdsToRecords.get(recId)}.map{case Some(record) => record}
+      some.flatMap { recId => IdsToRecords.get(recId) }
   }
 
   override def removeTag(tagId: Int, recordId: Int): Future[Unit] = Future{
